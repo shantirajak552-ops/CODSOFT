822d6363a4831586c30472debf8292238d457a56
Shanti Rajak <shantirajak552@gmail.com>
2026-03-24 11:47:04 +0530

    Uploaded calculator.py file
    Simple calculator using Python

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+op = input("Enter operation (+,-,*,/): ")
+
+if op == "+":
+    print(a + b)
+
+elif op == "-":
+    print(a - b)
+
+elif op == "*":
+    print(a * b)
+
+elif op == "/":
+    print(a / b)
